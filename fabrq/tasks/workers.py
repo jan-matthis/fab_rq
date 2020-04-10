02ae963d7ac55d359f564c3b58394d09ee335871
Jan-Matthis <mail@jan-matthis.de>
2020-04-10 19:54:15 +0200

    Support launch in tmux

--- fabrq/tasks/workers.py
+++ fabrq/tasks/workers.py
@@ -32,22 +32,40 @@ def start(c,
     queue=None,
     conda_env=None,
     num_workers=None,
+    burst=False,
+    tmux=True,
     ):
     assert queue is not None, "Specify queue"
     assert conda_env is not None, "Specify conda env"
     assert num_workers is not None, "Specify number of workers"
 
-    for n in range(int(num_workers)):
+    source_bashrc = f"source ~/.bashrc"
+    set_env_vars = "export MKL_NUM_THREADS=1; export NUMEXPR_NUM_THREADS=1; export OMP_NUM_THREADS=1"
+    activate_venv = f"conda activate {conda_env}"
+
+    if tmux:
+        # Create session if it does not exist
+        session_name = "rq_workers"
+        c.run(f"tmux has-session -t {session_name} || tmux new-session -d -s {session_name}")
+
+    for n in range(1, int(num_workers) + 1):
         worker_name = f"rq_worker_{n}_`hostname`_`whoami`_`date +%s`"
-
-        screen_cmd = f"screen -S {worker_name} -d -m bash -l -c"
-        source_bashrc = f"source ~/.bashrc"
-        set_env_vars = "export MKL_NUM_THREADS=1; export NUMEXPR_NUM_THREADS=1; export OMP_NUM_THREADS=1"
-        activate_venv = f"conda activate {conda_env}"
         start_rq_worker = f"rq worker {queue} --url {REDIS_URL} --name {worker_name}"
+        if burst:
+            start_rq_worker += " --burst"
 
-        cmd = f"{screen_cmd} '{source_bashrc}; {set_env_vars}; {activate_venv}; {start_rq_worker}'"
-        c.run(cmd)
+        if tmux:
+            t = f"{session_name}:{n}"
+            c.run(f"tmux new-window -d -k -t {t} -n rq{n}")
+            c.run(f"tmux send-keys -t {t} 'bash -l' C-m")
+            c.run(f"tmux send-keys -t {t} '{source_bashrc}' C-m")
+            c.run(f"tmux send-keys -t {t} '{set_env_vars}' C-m")
+            c.run(f"tmux send-keys -t {t} '{activate_venv}' C-m")
+            c.run(f"tmux send-keys -t {t} '{start_rq_worker}' C-m")
+        else:
+            screen_cmd = f"screen -S {worker_name} -d -m bash -l -c"
+            cmd = f"{screen_cmd} '{source_bashrc}; {set_env_vars}; {activate_venv}; {start_rq_worker}'"
+            c.run(cmd)
 
 
 @task_fabric
